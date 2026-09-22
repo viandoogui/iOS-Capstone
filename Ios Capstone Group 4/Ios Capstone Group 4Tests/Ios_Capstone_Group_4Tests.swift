4fe6326c8a5faf35a011216abbaa9ffb4d8eb44f
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2026-09-21 19:01:47 -0500

    Creates Initial Project

---
+++ Ios Capstone Group 4/Ios Capstone Group 4Tests/Ios_Capstone_Group_4Tests.swift
@@ -0,0 +1,19 @@
+//
+//  Ios_Capstone_Group_4Tests.swift
+//  Ios Capstone Group 4Tests
+//
+//  Created by user303000 on 9/21/26.
+//
+
+import Testing
+@testable import Ios_Capstone_Group_4
+
+struct Ios_Capstone_Group_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ Ios Capstone Group 4/Ios Capstone Group 4UITests/Ios_Capstone_Group_4UITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  Ios_Capstone_Group_4UITestsLaunchTests.swift
+//  Ios Capstone Group 4UITests
+//
+//  Created by user303000 on 9/21/26.
+//
+
+import XCTest
+
+final class Ios_Capstone_Group_4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
